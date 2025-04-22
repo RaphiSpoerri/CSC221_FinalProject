08851c32ed47782390a76b77aa3f9ac4cc46d9af
Raphael Spoerri <coolrustacean@gmail.com>
2025-04-22 09:54:08 -0400

    added javadoc and methods

--- Budget.java
+++ Budget.java
@@ -1,4 +1,72 @@
 
-public class Budget {
-    
+class Account {}
+/**
+ * The {@code Budget} class will handle all budget related operations such as transactions, updating, creating, deleting, and reading in from CSV files
+ *  @author Shaeem Rockcliffe
+ *  @author Raphael Spoerri
+ *  @author Giovanni Carrion
+ *  @author John Ortega
+ *  @version %I%, %G%
+ */
+class Budget {
+    /**
+     * Constructs a Budget instance for a specific account
+     * @params account is the account that will be associated with this budget instance. A valid account needs to be passed in order to create a budget instance.
+     */
+    Budget(Account account) { }
+    /**
+     * Represents one transaction, i.e. one row in the CSV file of transactions.
+     * @author Shaeem Rockcliffe
+     * @author Raphael Spoerri
+     * @author Giovanni Carrion
+     * @author John Ortega
+     * @version %I%, %G%
+     */
+    public static class Transaction {
+        /**
+         * Returns the date the transaction took place, in the format MM/DD/YYYY.
+         * @return the date of the transaction
+         */
+        public String getDate() { }
+        /**
+         * Returns a string categorizing the transaction.
+         * @return the transaction category
+         */
+        public String getCategory() { }
+        /**
+         * Returns the net change (in cents) to the user's bank account, positive if money was added and negative if money was spent.
+         * @return the net change
+         */
+        public long getAmount() { }
+    }
+    /**
+     * Prompts the user for the year number for creating or 
+     * updating the user's file.
+     * The implementation of how the prompt is displayed and
+     * how the user's choice is handled is left to the implementing 
+     * class.
+     */
+    void promptToCreateOrUpdate() {}
+    /**
+     * Prompts the user for the year number of the file to
+     * delete.
+     * The implementation of how the prompt is displayed and
+     * how the user's choice is handled is left to the implementing 
+     * class.
+     */
+    void promptToDelete();
+    /**
+     * Reads transaction data from a CSV file for a specific year.
+     * @param year the year for which transactions are to be read
+     * @return an ArrayList of Transaction objects for the given year
+     */
+    ArrayList<Transaction> readCSV(int year);
+    /**
+     * Retrieves a list of all years for which transaction data exists.
+     * @return an ArrayList of Integer years
+     */
+    ArrayList<Integer> getYears();
 }
+
+
+
