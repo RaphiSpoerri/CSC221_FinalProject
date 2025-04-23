3bbc3e8c1b14d88e8a395b9d813bf8a20cf607fe
Raphi Spoerri <140029470+RaphiSpoerri@users.noreply.github.com>
2025-04-22 20:51:05 -0400

    Update Budget.java

--- Budget.java
+++ Budget.java
@@ -16,7 +16,7 @@ class Budget {
      * Constructs a Budget instance for a specific account
      * @params account is the account that will be associated with this budget instance. A valid account needs to be passed in order to create a budget instance.
      */
-    Budget(Account account) { }
+    Budget(Account account) { /* not implemented yet */ }
     /**
      * Represents one transaction, i.e. one row in the CSV file of transactions.
      * @author Shaeem Rockcliffe
@@ -26,21 +26,23 @@ class Budget {
      * @version %I%, %G%
      */
     public static class Transaction {
+        
+        public Transaction(String date, String category, long amount) { /* not implemented yet */ }
         /**
          * Returns the date the transaction took place, in the format MM/DD/YYYY.
          * @return the date of the transaction
          */
-        public String getDate() { return null; }
+        public String getDate() { /* not implemented yet */ return null; }
         /**
          * Returns a string categorizing the transaction.
          * @return the transaction category
          */
-        public String getCategory() { return null;  }
+        public String getCategory() { /* not implemented yet */ return null;  }
         /**
          * Returns the net change (in cents) to the user's bank account, positive if money was added and negative if money was spent.
          * @return the net change
          */
-        public long getAmount() { return 0;  }
+        public long getAmount() { /* not implemented yet */ return 0;  }
     }
     /**
      * Prompts the user for the year number for creating or 
@@ -49,7 +51,7 @@ class Budget {
      * how the user's choice is handled is left to the implementing 
      * class.
      */
-    void promptToCreateOrUpdate() { }
+    void promptToCreateOrUpdate() { /* not implemented yet */ }
     /**
      * Prompts the user for the year number of the file to
      * delete.
@@ -57,18 +59,18 @@ class Budget {
      * how the user's choice is handled is left to the implementing 
      * class.
      */
-    void promptToDelete() { }
+    void promptToDelete() { /* not implemented yet */ }
     /**
      * Reads transaction data from a CSV file for a specific year.
      * @param year the year for which transactions are to be read
      * @return an ArrayList of Transaction objects for the given year
      */
-    ArrayList<Transaction> readCSV(String year) { return null; }
+    ArrayList<Transaction> readCSV(String year) { /* not implemented yet */ return null; }
     /**
      * Retrieves a list of all years for which transaction data exists.
      * @return an ArrayList of String years
      */
-    ArrayList<String> getYears() { return null; }
+    ArrayList<String> getYears() { /* not implemented yet */ return null; }
 }
 
 
